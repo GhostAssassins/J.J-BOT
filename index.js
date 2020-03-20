794ca7a350876ac7b4f59c62713e3a3aef0dbe55
GhostAssassins <62435936+GhostAssassins@users.noreply.github.com>
2020-03-20 16:24:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+const bot = new discord.Client();
+
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is online!!!`)
+    
+    bot.user.setActivity("Porn", {type: "WATCHING"})
+})
+
+bot.login(botConfig.token);
